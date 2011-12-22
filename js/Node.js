77c1fc08aca4e77e048410ecd73b850759d6cd7d
Erik Möller <emoller@opera.com>
2011-12-22 11:52:14 +0100

    Don't perform animation blending when not needed.

--- js/Node.js
+++ js/Node.js
@@ -139,31 +139,30 @@ function NodePrototype() {
         // Current animation
         var t1 = this.getAnimatedTransformation(scene.animation);
 
-        // Weight factors for the fade in/out animations
-        var w1 = 1.0;
-        var fadeAnim = null;
         if (scene.animation.fadeTime < scene.animation.fadeMax && scene.animation.fadeAnim) {
-            fadeAnim = scene.animation.fadeAnim;
-            w1 = scene.animation.fadeTime / scene.animation.fadeMax;
+            // Weight factors for the fade in/out animations
+            var fadeAnim = scene.animation.fadeAnim;
+            var w1 = scene.animation.fadeTime / scene.animation.fadeMax;
+            var w2 = 1.0 - w1;
+
+            // Old animation
+            var t2 = this.getAnimatedTransformation(fadeAnim);
+
+            // Blend animations
+            var translation = [w1 * t1.translation[0] + w2 * t2.translation[0],
+                               w1 * t1.translation[1] + w2 * t2.translation[1],
+                               w1 * t1.translation[2] + w2 * t2.translation[2]];
+            var rotation = [w1 * t1.rotation[0] + w2 * t2.rotation[0],
+                            w1 * t1.rotation[1] + w2 * t2.rotation[1],
+                            w1 * t1.rotation[2] + w2 * t2.rotation[2]];
+            var scale = [w1 * t1.scale[0] + w2 * t2.scale[0],
+                         w1 * t1.scale[1] + w2 * t2.scale[1],
+                         w1 * t1.scale[2] + w2 * t2.scale[2]];
+            var visibility = t1.visibility || t2.visibility;
+            this.calculateLocalTransform(translation, rotation, scale, [1.0 / acc.getX().magnitude(), 1.0 / acc.getY().magnitude(), 1.0 / acc.getZ().magnitude()], visibility);
+        } else {
+            this.calculateLocalTransform(t1.translation, t1.rotation, t1.scale, [1.0 / acc.getX().magnitude(), 1.0 / acc.getY().magnitude(), 1.0 / acc.getZ().magnitude()], t1.visibility);
         }
-        var w2 = 1.0 - w1;
-
-        // Old animation
-        var t2 = this.getAnimatedTransformation(fadeAnim);
-
-        // Blend animations
-        var translation = [w1 * t1.translation[0] + w2 * t2.translation[0],
-                           w1 * t1.translation[1] + w2 * t2.translation[1],
-                           w1 * t1.translation[2] + w2 * t2.translation[2]];
-        var rotation = [w1 * t1.rotation[0] + w2 * t2.rotation[0],
-                        w1 * t1.rotation[1] + w2 * t2.rotation[1],
-                        w1 * t1.rotation[2] + w2 * t2.rotation[2]];
-        var scale = [w1 * t1.scale[0] + w2 * t2.scale[0],
-                     w1 * t1.scale[1] + w2 * t2.scale[1],
-                     w1 * t1.scale[2] + w2 * t2.scale[2]];
-        var visibility = t1.visibility || t2.visibility;
-
-        this.calculateLocalTransform(translation, rotation, scale, [1.0 / acc.getX().magnitude(), 1.0 / acc.getY().magnitude(), 1.0 / acc.getZ().magnitude()], visibility);
 
         var accumulated = acc.copy().multiply(this.getLocalTransform());
 
